9fedcb8dcf0a8d933bcdb776cef3169d7e51c363
Rishabh Jain <jainrishabh8153@gmail.com>
2026-07-08 15:16:34 +0530

    .

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,11 +1,14 @@
-/** @type {import("eslint").Linter.Config} */
-const config = {
+import { nextJsConfig } from "@repo/eslint-config/next-js";
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+  ...nextJsConfig,
+  {
     rules: {
       "no-case-declarations": "off", // Disable 'no-case-declarations' warnings
       "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }], // Ignore unused variables starting with '_'
-      "turbo/no-undeclared-env-vars": "off", // Disable undeclared env vars warning (or explicitly declare them)
+      "turbo/no-undeclared-env-vars": "off", // Disable undeclared env vars warning
     },
-  };
-  
-  export default config;
+  }
+];
   
